f66042cbfbe919d4a6eabd2ecdd6b3598f8aca2e
jakgondzik <149070003+jakgondzik@users.noreply.github.com>
2025-06-15 19:50:55 +0200

    d

--- BazaAudyt/Models/LPA_Pytania.cs
+++ BazaAudyt/Models/LPA_Pytania.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public string? Pytanie { get; set; }
-        public string? Obszar { get; set; }
+        public string Obszar { get; set; }
         public int? Nr {  get; set; }
-        public bool? Aktywne { get; set; }
+        public bool Aktywne { get; set; }
         public string? Norma { get; set; }
         public int? Waga { get; set; }
     }
